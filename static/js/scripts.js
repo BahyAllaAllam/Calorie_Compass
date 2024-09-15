b553f39870f0c53497ac2bdb590ceb8a3c0ef262
BahyAlaaAllam <bahy.alam@yahoo.com>
2024-09-15 16:19:30 +0300

    modifying the base to be more readable add the correct path to the background image

--- static/js/scripts.js
+++ static/js/scripts.js
@@ -10,26 +10,30 @@ document.addEventListener('DOMContentLoaded', function() {
     } else {
         passwordField = document.getElementById('id_password1')
     }
-    eyeIcon.addEventListener('click', function() {
-        if (passwordField.type === 'password') {
-            passwordField.type = 'text';
-            eyeIcon.classList.remove('fa-eye');
-            eyeIcon.classList.add('fa-eye-slash'); // Change to eye-slash icon
-        } else {
-            passwordField.type = 'password';
-            eyeIcon.classList.remove('fa-eye-slash');
-            eyeIcon.classList.add('fa-eye'); // Change back to eye icon
-        }
-    });
-    eyeIcon_oldpassword.addEventListener('click', function() {
-        if (oldpasswordField.type === 'password') {
-            oldpasswordField.type = 'text';
-            eyeIcon_oldpassword.classList.remove('fa-eye');
-            eyeIcon_oldpassword.classList.add('fa-eye-slash'); // Change to eye-slash icon
-        } else {
-            oldpasswordField.type = 'password';
-            eyeIcon_oldpassword.classList.remove('fa-eye-slash');
-            eyeIcon_oldpassword.classList.add('fa-eye'); // Change back to eye icon
-        }
-    });
+    if (eyeIcon){
+        eyeIcon.addEventListener('click', function() {
+            if (passwordField.type === 'password') {
+                passwordField.type = 'text';
+                eyeIcon.classList.remove('fa-eye');
+                eyeIcon.classList.add('fa-eye-slash'); // Change to eye-slash icon
+            } else {
+                passwordField.type = 'password';
+                eyeIcon.classList.remove('fa-eye-slash');
+                eyeIcon.classList.add('fa-eye'); // Change back to eye icon
+            }
+        });
+    }
+    if (eyeIcon_oldpassword){
+        eyeIcon_oldpassword.addEventListener('click', function() {
+            if (oldpasswordField.type === 'password') {
+                oldpasswordField.type = 'text';
+                eyeIcon_oldpassword.classList.remove('fa-eye');
+                eyeIcon_oldpassword.classList.add('fa-eye-slash'); // Change to eye-slash icon
+            } else {
+                oldpasswordField.type = 'password';
+                eyeIcon_oldpassword.classList.remove('fa-eye-slash');
+                eyeIcon_oldpassword.classList.add('fa-eye'); // Change back to eye icon
+            }
+        });
+    }
 });
