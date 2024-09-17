513bc95acfcaf98446103571eb0d2dc2690e58ec
BahyAlaaAllam <bahy.alam@yahoo.com>
2024-09-17 14:01:40 +0300

    add the back to top button

--- static/js/meals_tracking.js
+++ static/js/meals_tracking.js
@@ -78,3 +78,31 @@ document.getElementById('add-meal-button').addEventListener('click', function(ev
         }
     });
 });
+
+//Get the button:
+mybutton = document.getElementById("myBtn");
+
+// Add an event listener to trigger the scroll to top function when the button is clicked
+mybutton.addEventListener('click', topFunction);
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {
+    scrollFunction()
+};
+
+function scrollFunction() {
+  if (window.scrollY > 20) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+    window.scrollTo ({
+        top:0,
+        left:0,
+        behavior: "smooth"
+    });
+
+}
